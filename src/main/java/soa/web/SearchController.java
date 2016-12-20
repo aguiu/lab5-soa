6a9d0070ebdb5f013f4f6d80b919453829bb6c87
Alejandro Guiu <aguiuperez@gmail.com>
2016-12-20 19:24:22 +0100

    max:n implemented and properties encrypted

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.HashMap;
 
 
 @Controller
@@ -23,6 +24,14 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+		HashMap<String,Object> h = new HashMap<>();
+		if (q.contains("max:")) {
+			// example: "unizar max:4" -> parts[0]="unizar", parts[1]=4
+			String[] parts = q.split("max:");
+			q = parts[0];			
+			h.put("CamelTwitterCount", Integer.parseInt(parts[1]));
+		}
+		h.put("CamelTwitterKeywords", q);
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", h);
     }
 }
